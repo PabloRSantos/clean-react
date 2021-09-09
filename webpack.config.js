7f9f3afa6d8c5a0f206687ae0da9422aa1096b85
Pablo <pablorsantos15@gmail.com>
2021-09-08 23:25:27 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,61 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.join(__dirname, 'public/js'),
-        publicPath: '/public/js',
-        filename: 'bundle.js'
+  mode: "development",
+  entry: path.join(__dirname, "src", "main", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "public", "js"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".scss"],
+    alias: {
+      "@": path.join(__dirname, "src"),
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', 'scss'],
-        alias: {
-            '@': path.join(__dirname, 'src')
-        }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: "./public",
     },
-    module: {
-        rules: [{
-            test: /\.ts(x?)$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true
-                    }
-                },
-                {
-                    loader: 'sass-loader'
-                }
-            ],
-            exclude: /node_modules/
-        }
-    ]
-    },
-    devServer: {
-        contentBase: './public',
-        writeToDisk: true,
-        historyApiFallback: true,
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-    }, // nao inclui no bundle
-    plugins: [new CleanWebpackPlugin()],
-
-}
+    open: true,
+    historyApiFallback: true,
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  }, // nao inclui no bundle
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
+};
