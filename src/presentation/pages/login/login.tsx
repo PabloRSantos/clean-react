13a34a5c5b4391c61d8d56ea1131e2a0d29b0890
Pablo <pablorsantos15@gmail.com>
2021-09-09 23:55:05 -0300

    feat: ensure Login present erro and hide spinner on authentication failure

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,10 @@
-
 import { Authentication } from '@/domain/usecases'
-import { Input, FormStatus, Footer, LoginHeader } from '@/presentation/components'
+import {
+  Input,
+  FormStatus,
+  Footer,
+  LoginHeader
+} from '@/presentation/components'
 import { FormContext } from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import React, { FormEvent, useEffect, useState } from 'react'
@@ -9,7 +13,7 @@ import Styles from './login-styles.scss'
 type Props = {
   validation: Validation
   authentication: Authentication
-}
+};
 
 export const Login: React.FC<Props> = ({ validation, authentication }) => {
   const [state, setState] = useState({
@@ -29,24 +33,51 @@ export const Login: React.FC<Props> = ({ validation, authentication }) => {
     })
   }, [state.email, state.password])
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSubmit = async (
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault()
 
-    if (state.isLoading || state.emailError || state.passwordError) return
-    setState({ ...state, isLoading: true })
+    try {
+      if (state.isLoading || state.emailError || state.passwordError) return
+      setState({ ...state, isLoading: true })
 
-    await authentication.auth({ email: state.email, password: state.password })
+      await authentication.auth({
+        email: state.email,
+        password: state.password
+      })
+    } catch (error) {
+      setState({
+        ...state,
+        isLoading: false,
+        mainError: error.message
+      })
+    }
   }
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <FormContext.Provider value={{ state, setState }}>
-        <form data-testid='form' className={Styles.form} onSubmit={handleSubmit}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
-          <Input type="password" name="password" placeholder="Digite sua senha" />
-          <button className={Styles.submit} disabled={!!state.emailError || !!state.passwordError} type="submit">Entrar</button>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <button
+            className={Styles.submit}
+            disabled={!!state.emailError || !!state.passwordError}
+            type="submit"
+          >
+            Entrar
+          </button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
         </form>
