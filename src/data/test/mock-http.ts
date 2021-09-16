4f2bd0f2483975d2cf39a0fcece634a01d483f32
Pablo <pablorsantos15@gmail.com>
2021-09-16 09:58:53 -0300

    test: ensure RemoteLoadSurveyList call HttpGetClient with correct URL

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,7 +1,7 @@
-import { HttpPostClient, HttpResponse, HttpStatusCode, HttpPostParams } from '@/data/protocols/http'
+import { HttpPostClient, HttpResponse, HttpStatusCode, HttpPostParams, HttpGetClient, HttpGetParams } from '@/data/protocols/http'
 import faker from 'faker'
 
-export class HttpPostClientSpy<R> implements HttpPostClient<R> {
+export class HttpPostClientSpy<R = any> implements HttpPostClient<R> {
   url?: string
   body?: any
   response: HttpResponse<R> = {
@@ -13,6 +13,15 @@ export class HttpPostClientSpy<R> implements HttpPostClient<R> {
     this.body = params.body
 
     return Promise.resolve(this.response)
+  }
+}
+
+export class HttpGetClientSpy<R = any> implements HttpGetClient<R> {
+  url?: string
+  async get (params: HttpGetParams): Promise<HttpResponse<R>> {
+    this.url = params.url
+
+    return Promise.resolve(null)
   }
 }
 
