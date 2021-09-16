697f6eed6a6747da508c821da157b25c7622ce41
Pablo <pablorsantos15@gmail.com>
2021-09-16 17:27:18 -0300

    feat: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,6 +1,12 @@
-import { SetStorage } from '@/data/protocols/cache'
+import { GetStorage, SetStorage } from '@/data/protocols/cache'
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, GetStorage {
+  get (key: string): any {
+    const data = localStorage.getItem(key)
+
+    return JSON.parse(data)
+  }
+
   set (key: string, value: any): void {
     localStorage.setItem(key, JSON.stringify(value))
   }
