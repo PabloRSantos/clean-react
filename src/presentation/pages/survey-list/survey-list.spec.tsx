7b53af06d3452a677bb7cb97b1e32b315cc07d80
Pablo <pablorsantos15@gmail.com>
2021-09-17 11:01:52 -0300

    test: ensure SurveyList render Error on failure

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { LoadSurveyListSpy } from '@/presentation/test/mock-load-survey-list'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '..'
+import { UnexpectedError } from '@/domain/errors'
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy
 }
 
-const makeSut = (): SutTypes => {
-  const loadSurveyListSpy = new LoadSurveyListSpy()
+const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
   render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
 
   return { loadSurveyListSpy }
@@ -19,6 +19,7 @@ describe('SurveyListComponent', () => {
     makeSut()
     const surveyList = screen.getByRole('list')
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    expect(screen.queryByRole('button', { name: /recarregar/i })).not.toBeInTheDocument()
   })
 
   test('Should call LoadSurveyList', () => {
@@ -30,5 +31,17 @@ describe('SurveyListComponent', () => {
     const { loadSurveyListSpy } = makeSut()
     const surveyItems = await screen.findAllByTestId('survey-item')
     expect(surveyItems).toHaveLength(loadSurveyListSpy.surveys.length)
+    expect(screen.queryByRole('button', { name: /recarregar/i })).not.toBeInTheDocument()
+  })
+
+  test('Should render error on failure', async () => {
+    const error = new UnexpectedError()
+
+    const loadSurveyListSpy = new LoadSurveyListSpy()
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(error)
+    makeSut(loadSurveyListSpy)
+
+    expect(await screen.findByText(error.message)).toBeInTheDocument()
+    expect(screen.queryByRole('list')).not.toBeInTheDocument()
   })
 })
