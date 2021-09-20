79fa93dad60184986629b9c099da3ed935366708
Pablo <pablorsantos15@gmail.com>
2021-09-20 14:09:34 -0300

    test: ensure RemoteLoadSurveyResult calls HttpGetClient with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,26 @@
+import { HttpGetClientSpy } from '@/data/test'
+import faker from 'faker'
+import { RemoteLoadSurveyResult } from './remote-load-survey-result'
+
+type SutTypes = {
+  sut: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+
+  return { sut, httpGetClientSpy }
+}
+
+describe('RemoteLoadSurveyResult', () => {
+  test('Should call HttpGetClient with correct URL', async () => {
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+
+    sut.load()
+
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+})
