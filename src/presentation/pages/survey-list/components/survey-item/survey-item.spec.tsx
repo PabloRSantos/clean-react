d786bf06d7be63cad53bf985726b5bddea5ac926
Pablo <pablorsantos15@gmail.com>
2021-09-21 11:32:56 -0300

    fix: move Calendar tests back to SurveyList

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -12,22 +12,39 @@ describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = {
       ...mockSurveyModel(),
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00')
     }
     makeSut(survey)
 
     expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByText(survey.question)).toBeInTheDocument()
+    const spanDay = screen.getByText('10')
+    const spanMonth = screen.getByText('jan')
+    const spanYear = screen.getByText('2020')
+
+    expect(spanDay).toBeInTheDocument()
+    expect(spanMonth).toBeInTheDocument()
+    expect(spanYear).toBeInTheDocument()
   })
 
   test('Should render with correct values', () => {
     const survey = {
       ...mockSurveyModel(),
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2019-05-03T00:00:00')
     }
     makeSut(survey)
 
     expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByText(survey.question)).toBeInTheDocument()
+
+    const spanDay = screen.getByText('03')
+    const spanMonth = screen.getByText('mai')
+    const spanYear = screen.getByText('2019')
+
+    expect(spanDay).toBeInTheDocument()
+    expect(spanMonth).toBeInTheDocument()
+    expect(spanYear).toBeInTheDocument()
   })
 })
