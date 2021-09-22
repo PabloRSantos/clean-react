7037dae43e523e1bbe287e599b2120fc2a2792a3
Pablo <pablorsantos15@gmail.com>
2021-09-22 14:13:09 -0300

    test: ensure SurveyResult present new result on answer click

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -184,7 +184,9 @@ describe('SurveyResult Component', () => {
     const answersWrap = screen.queryAllByTestId('answer-wrap')
     fireEvent.click(answersWrap[1])
 
-    expect(await screen.findByText(error.message)).toBeInTheDocument()
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    expect(screen.queryByText(error.message)).toBeInTheDocument()
     expect(screen.queryByTestId('question')).not.toBeInTheDocument()
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
   })
@@ -204,5 +206,48 @@ describe('SurveyResult Component', () => {
     await waitFor(() => screen.getByTestId('survey-result'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(history.location.pathname).toBe('/login')
+  })
+
+  test('Should present SurveyResult data on SaveSurveyResult success', async () => {
+    const saveSurveyResultSpy = new SaveSurveyResultSpy()
+    const surveyResult = {
+      ...mockSurveyResultModel(),
+      date: new Date('2018-02-20T00:00:00')
+    }
+    saveSurveyResultSpy.surveyResult = surveyResult
+    makeSut({ saveSurveyResultSpy })
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    const answersWrap = screen.queryAllByTestId('answer-wrap')
+    fireEvent.click(answersWrap[1])
+
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    expect(screen.getByText('20')).toBeInTheDocument()
+    expect(screen.getByText('fev')).toBeInTheDocument()
+    expect(screen.getByText('2018')).toBeInTheDocument()
+    expect(screen.getByText(surveyResult.question)).toBeInTheDocument()
+    expect(screen.getByTestId('answers').childElementCount).toBe(2)
+
+    expect(answersWrap[0]).toHaveClass('active')
+    expect(answersWrap[1]).not.toHaveClass('active')
+
+    const images = screen.queryAllByTestId('answer-image')
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0]).toHaveTextContent(
+      `${surveyResult.answers[0].percent}%`
+    )
+    expect(percents[1]).toHaveTextContent(
+      `${surveyResult.answers[1].percent}%`
+    )
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
   })
 })
